c467bcf5e6a5ceb6dd83e1476f2ca306c2250ddd
jsonsong <sj178220709@163.com>
2015-10-30 14:55:20 +0800

    修复help bug

--- myos.js
+++ myos.js
@@ -18,7 +18,7 @@ program
 
 
 //添加额外的文档描述
-program.on('help', function () {
+program.on('--help', function () {
     console.log("my defined work :");
     _.each(cmdKeys, function (key) {
         console.log(key)
